01e8d73a942edceb2283eef95c8414c1449dcc2d
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-25 02:05:33 -0300

    feat(app/page.tsx): add section, h1 description and p with spme styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const Page = () => {
-  return <h1>NextEvent</h1>;
+  return (
+    <section>
+      <h1 className="text-center">
+        O hub de todo evento dev <br /> que você não pode perder
+      </h1>
+      <p className="text-center mt-5">
+        Hackathons, meetups e conferências, tudo em um só lugar.
+      </p>
+    </section>
+  );
 };
 
 export default Page;
