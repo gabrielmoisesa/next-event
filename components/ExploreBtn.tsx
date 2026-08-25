e34641a8b800ce9334384fd340e162651842d413
gabrielmoisesa <gabrielmoisesalves18@gmail.com>
2026-08-25 02:24:30 -0300

    feat(explorebtn.tsx): add button properties, style, text and icon

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,5 +1,20 @@
+'use client';
+
+import Image from 'next/image.js';
+
 const ExploreBtn = () => {
-  return <button>ExploreBtn</button>;
+  return (
+    <button type="button" id="explore-btn" className="mt-7 mx-auto">
+      <a href="#events">Explorar Eventos</a>
+      <Image
+        src="../icons/arrow-down.svg"
+        alt="arrow-down"
+        width={24}
+        height={24}
+        className="ml-1"
+      />
+    </button>
+  );
 };
 
 export default ExploreBtn;
